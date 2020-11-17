99c930c59ede581f3ea4952ec5dff31380c1eed9
pedroPolonea <phpolonea@hotmail.com>
2020-11-16 23:05:22 -0300

    init

---
+++ src/main/java/com/php/rm/PocRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.php.rm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/php/rm/PocRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.php.rm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
